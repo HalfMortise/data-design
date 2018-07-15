d6072ecf8a0c452203e8339c2fce93ae57e731c2
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-15 17:38:02 -0600

    Fixed table names

--- epic/data-design.sql
+++ epic/data-design.sql
@@ -1,16 +1,17 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 -- The statement below sets the collation of the database to utf8
-ALTER DATABASE your_database_name_CHANGE_ME CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE ECORSI CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- this is a comment in SQL (yes, the space is needed!)
 -- the following statements will drop the tables and re-add them
 -- this is akin to reformatting and reinstalling Windows (OS X never needs a reinstall...) ;)
 -- never ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever
 -- do this on live data!!!!
-DROP TABLE IF EXISTS `like`;
-DROP TABLE IF EXISTS tweet;
+DROP TABLE if EXISTS contentprofile
+DROP TABLE IF EXISTS content;
 DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS account;
 
 -- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
 CREATE TABLE profile (
@@ -48,7 +49,7 @@ CREATE TABLE content (
 );
 
 -- create the like entity (a weak entity from an m-to-n for profile --> tweet)
-CREATE TABLE contentProfile (
+CREATE TABLE contentprofile (
 	-- these are still foreign keys
 	contentProfileProfileId BINARY(16) NOT NULL,
 	contentProfileContentId BINARY(16) NOT NULL,
