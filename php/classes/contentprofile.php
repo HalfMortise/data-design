7936ac9c3bfb13a6552776d68694715a61452359
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 22:25:27 -0600

    Added class content; accessors and mutators

--- php/classes/contentprofile.php
+++ php/classes/contentprofile.php
@@ -2,12 +2,27 @@
 /***/
 //this defines the class's state and behavior
 class Contentprofile {
+/**
+ * id for the content profile; this is the primary key
+ * @var Uuid $profileId
+**/
 	private $contentProfileProfileId;
+
+/**
+ * id for
+ * @var Uuid $contentProfileContentId
+**/
 	private $contentProfileContentId;
+
+/**
+ * id for
+ * @var Uuid $contentProfileTrackPosition
+**/
 	private $contentProfileTrackPosition;
 
-//this is the accessor method to allow to "get"
-//@return UUID values
+/**this is the accessor method to allow to "get"
+ * @return UUID values
+**/
 	public function getContentProfileProfileId(): Uuid {
 		return ($this->contentProfileProfileId);
 	}
