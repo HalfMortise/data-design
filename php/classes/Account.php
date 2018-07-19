39c6223f87b1a88823b9e7e9c36bc651546817cb
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-19 14:29:07 -0600

    Updated with ValidateUuid, ValidateDate; updated accountName and accountEmail mutator methods

--- php/classes/Account.php
+++ php/classes/Account.php
@@ -1,17 +1,25 @@
 <?php
-/***/
-
+/**
+ * Class identified as Account
+ *
+ * @author HalfMortise
+ * @version 2.0
+ **/
 class Account {
+   use ValidateDate;
+   use ValidateUuid;
+
+
    /**
     * id for account; this is the primary key
     * @var Uuid $accountId
-   **/
+    **/
    private $accountId;
 
    /**
     * profile id for account
     * @var Uuid $accountProfileId
-   **/
+    **/
    private $accountProfileId;
    /**
     * id for account activation token
@@ -36,6 +44,44 @@ class Account {
     * @var Uuid $accountHash
     **/
    private $accountHash;
+
+
+
+   /**
+    * Constructor for this class
+    *
+    * @param string|Uuid $newAccountId id of this Account
+    * @param string|Uuid $newAccountProfileId id of the profile that contains the Account
+    * @param string|Uuid $newAccountActivationToken token used to activate the Account
+    * @param string|Uuid $newAccountName name assigned to Account
+    * @param string|Uuid $newAccountEmail email assigned to Account
+    * @param string|Uuid $newAccountHash hash used for Account password creation
+    * @param \DateTime|string|null $newAccountDate date and time Account was created
+    * @throws \InvalidArgumentException if data types are not valid
+    * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+    * @throws \TypeError if data types violate type hints
+    * @throws \Exception if some other exception occurs
+    * @Documentation https://php.net/manual/en/language.oop5.decon.php
+   **/
+
+   public function __construct($newAccountId, $newAccountProfileId, $newAccountActivationToken, string $newAccountName, string $newAccountEmail, $newAccountHash) {
+      try {
+         $this->setAccountId($newAccountId);
+         $this->setAccountProfileId($newAccountProfileId);
+         $this->setAccountActivationToken($newAccountActivationToken);
+         $this->setAccountName($newAccountName);
+         $this->setAccountEmail($newAccountEmail);
+         $this->setAccountHash($newAccountHash);
+      }
+
+      //exception type
+      catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+      $exceptionType = get_class($exception);
+      throw(new $exceptionType($exception->getMessage(), 0, $exception));
+      }
+   }
+
+
 
    /**
     * accessor method for account id
@@ -64,7 +110,7 @@ class Account {
 
       //convert and store the profile id
       $this->accountId = $uuid;
-
+   }
      /**
       * accessor method for account profile id
       *
@@ -93,6 +139,7 @@ class Account {
 
          //convert and store the account profile id
          $this->accountProfileId = $uuid;
+      }
 
          /**
           * accessor method for account activation token
@@ -100,15 +147,15 @@ class Account {
           * @return Uuid value of account activation token
           **/
 
-         public function getActivationToken() : Uuid {
-            return($this->accountActivationToken);
+         public function getActivationToken(): Uuid {
+            return ($this->accountActivationToken);
          }
 
          /**
           * mutator method for account activation token
           *
           * @param Uuid $newAccountActivationToken new value of account activation token
-          * @throws \RangeException for $newAccountActivationToken
+          * @throws \RangeException if $newAccountActivationToken is not unique
           * @throws \TypeError if $newAccountActivationToken is not a uuid
           **/
 
@@ -122,6 +169,7 @@ class Account {
 
             //convert and store the account activation token
             $this->accountActivationToken = $uuid;
+            }
 
             /**
              * accessor method for account name
@@ -129,7 +177,7 @@ class Account {
              * @return Uuid value of account name
              **/
 
-            public function getAccountName() : Uuid {
+            public function getAccountName() : String {
                return($this->accountName);
             }
 
@@ -137,60 +185,54 @@ class Account {
              * mutator method for account name
              *
              * @param String $newAccountName new value of account name
-             * @throws \RangeException if $newAccountName is not unique
+             * @throws \RangeException if $newAccountName is not secure
              * @throws \TypeError if $newAccountName is not a string
              **/
 
-            public function setAccountId( $newAccountId) : void {
-               try {
-                  $string = self::validateString($newAccountName);
-               } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-                  $exceptionType = get_class($exception);
-                  throw(new $exceptionType($exception->getMessage(), 0, $exception));
+            public function setAccountName(string $newAccountName) : void {
+               //verify the account name is secure
+               $newAccountName = trim($newAccountName);
+               $newAccountName = filter_var($newAccountName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+               if(empty($newAccountName) === true) {
+                  throw(new \InvalidArgumentException("Account must have name"));
                }
 
-               //convert and store the profile name
-               $this->accountId = $string;
+               //convert and store the account name
+               $this->accountName = $newAccountName;
             }
-               /**
-                * accessor method for account name
-                *
-                * @return string value of account name
-                **/
-
-               public function getAccountName() : String {
-                  return($this->accountName);
-               }
 
             /**
              * accessor method for account email
              *
-             * @return String value of account email
+             * @return Uuid value of account email
              **/
+
             public function getAccountEmail() : String {
-               return($this->accountEmail);
+      return($this->accountEmail);
             }
 
             /**
-             * mutator method for account email
-             *
+            * mutator method for account email
+            *
              * @param String $newAccountEmail new value of account email
-             * @throws \RangeException if $newAccountEMail is not unique
-             * @throws \TypeError if $newAccountEmail is not a string
-             **/
-            public function setAccountEmail( $newAccountEmail) : void {
-               try {
-                  $string = self::validateString($newAccountEmail);
-               } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-                  $exceptionType = get_class($exception);
-                  throw(new $exceptionType($exception->getMessage(), 0, $exception));
-               }
+            * @throws \RangeException if $newAccountEmail is not secure
+           * @throws \TypeError if $newAccountEmail is not a string
+           **/
 
-               // convert and store the account email
-               $this->accountEmail = $string;
-            }
+           public function setAccountEmail(string $newAccountEmail) : void {
+               //verify the account email is secure
+               $newAccountEmail = trim($newAccountEmail);
+               $newAccountEmail = filter_var($newAccountEmail, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+             if(empty($newAccountEmail) === true) {
+                throw(new \InvalidArgumentException("Please provide email address"));
+             }
 
-            /**
+               //convert and store the account email
+               $this->accountEmail = $newAccountEmail;
+          }
+
+
+   /**
              * accessor method for account's password hash
              *
              * @return Uuid value of account's password hash
