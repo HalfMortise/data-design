b9c81453ae7074874dbe02fc204c10847a2533e4
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-22 22:25:20 -0600

    Added in wild cards and query template

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -39,24 +39,21 @@ class Profile {
       try {
          $this->setProfileId($newProfileId);
          $this->setProfileName($newProfileName);
-      }
-
-
-      //determine exception thrown
+      } //determine exception thrown
       catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
          $exceptionType = get_class($exception);
          throw(new $exceptionType($exception->getMessage(), 0, $exception));
       }
    }
-      //end exception
+   //end exception
 
    /**
     * accessor method for profile id
     *
     * @return Uuid value of profile id
     **/
-   public function getProfileId() : Uuid {
-      return($this->profileId);
+   public function getProfileId(): Uuid {
+      return ($this->profileId);
    }
 
    /**
@@ -66,7 +63,7 @@ class Profile {
     * @throws \RangeException if $newProfileId is not unique
     * @throws \TypeError if $newProfileId is not a uuid
     **/
-   public function setProfileId( $newProfileId) : void {
+   public function setProfileId($newProfileId): void {
       try {
          $uuid = self::validateUuid($newProfileId);
       } catch(\InvalidArgumentException | \RangeException | \ Exception | \ TypeError $exception) {
@@ -82,8 +79,8 @@ class Profile {
     *
     * @return String value of profile name
     **/
-   public function getProfileName() :string {
-      return($this->profileName);
+   public function getProfileName(): string {
+      return ($this->profileName);
    }
 
    /**
@@ -93,7 +90,7 @@ class Profile {
     * @throws \RangeException if $newProfileName is not secure
     * @throws \TypeError: none (leaving comment in for reference)
     **/
-   public function setProfileName(string $newProfileName) : void {
+   public function setProfileName(string $newProfileName): void {
       // verify the name content is secure
       $newProfileName = trim($newProfileName);
       $newProfileName = filter_var($newProfileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -111,7 +108,7 @@ class Profile {
     * @throws \PDOException when mySQL related errors occur
     * @throws \TypeError if $pdo is not a PDO connection object
     **/
-   public function insert(\PDO $pdo) : void {
+   public function insert(\PDO $pdo): void {
       // create query template
       $query = "INSERT INTO profile(profileId, profileName)";
       $statement = $pdo->prepare($query);
@@ -119,44 +116,44 @@ class Profile {
       $parameters = ["profileId" => $this->profileId->getBytes(), "profileName => $this->profileName"];
       $statement->execute($parameters);
 
-      }
-
-      /**
-       * deletes this Profile from mySQL
-       *
-       * @param \PDO $pdo PDO connection object
-       * @throws \PDOException when mySQL related errors occur
-       * @throws \TypeError if $pdo is not a PDO connection object
-       **/
-      public function delete(\PDO $pdo) : void {
-         // create query template
-         $query = "DELETE FROM profile WHERE profileId = :profileId";
-         $statement = $pdo->prepare($query);
-         // bind the member variables to the place holder in the template
-         $parameters = ["profileId" => $this->profileId->getBytes()];
-         $statement->execute($parameters);
-
-      }
-
-      /**
-       * updates this Profile in mySQL
-       *
-       * @param \PDO $pdo PDO connection object
-       * @throws \PDOException when mySQL related errors occur
-       * @throws \TypeError if $pdo is not a PDO connection object
-       **/
-
-      public function update(\PDO $pdo) : void {
-         // create a query template
-         $query = "UPDATE profile SET profileId = :profileId, profileName = :profileName";
-         $statement = $pdo->prepare($query);
-         $parameters = ["profileId" =>$this->profileId->getBytes(), "profileName" => $this->profileName];
-         $statement->execute($parameters);
-
-      }
+   }
 
    /**
-    * Gets the Profile by profile Id
+    * deletes this Profile from mySQL
+    *
+    * @param \PDO $pdo PDO connection object
+    * @throws \PDOException when mySQL related errors occur
+    * @throws \TypeError if $pdo is not a PDO connection object
+    **/
+   public function delete(\PDO $pdo): void {
+      // create query template
+      $query = "DELETE FROM profile WHERE profileId = :profileId";
+      $statement = $pdo->prepare($query);
+      // bind the member variables to the place holder in the template
+      $parameters = ["profileId" => $this->profileId->getBytes()];
+      $statement->execute($parameters);
+
+   }
+
+   /**
+    * updates this Profile in mySQL
+    *
+    * @param \PDO $pdo PDO connection object
+    * @throws \PDOException when mySQL related errors occur
+    * @throws \TypeError if $pdo is not a PDO connection object
+    **/
+
+   public function update(\PDO $pdo): void {
+      // create a query template
+      $query = "UPDATE profile SET profileId = :profileId, profileName = :profileName";
+      $statement = $pdo->prepare($query);
+      $parameters = ["profileId" => $this->profileId->getBytes(), "profileName" => $this->profileName];
+      $statement->execute($parameters);
+
+   }
+
+   /**
+    * Gets the Profile by profile Id (primary key)
     *
     * @param \PDO $pdo PDO connection object
     * @param Uuid|string $profileId profile id to search for
@@ -165,51 +162,66 @@ class Profile {
     * @throws \TypeError when a variable is not the correct data type
     **/
 
-      public static function getProfileByProfileId(\PDO $pdo, $profileId) : ?Profile {
-         //sanitize the profileId before searching
-         try {
-            $profileId = self::validateUuid($profileId);
-         } catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-            throw (new \PDOException($exception->getMessage(), 0, $exception));
-         }
-
-         //create a query template
-         $query = "SELECT profileId, accountProfileId FROM profile WHERE profileId = :profileId";
-         $statement= $pdo->prepare($query);
-
-         //bind the profile id to the place holder in the template
-         $parameters = ["profileId" => $profileId->getBytes()];
-         $statement->execute($parameters);
-
-         //grab the profile from mySQL
-         try {
-            $profile = null;
-            $statement->setFetchMode(\PDO::FETCH_ASSOC);
-            $row = $statement->fetch();
-            if($row !== false) {
-               $profile = new Profile($row["profileId"], $row["accountProfileId"]);
-            }
-         } catch(\Exception $exception) {
-            //if the row couldn't be converted, rethrow it
-            throw(new \PDOException($exception->getMessage(), 0, $exception));
-         }
-         return($profile);
+   public static function getProfileByProfileId(\PDO $pdo, $profileId): ?Profile {
+      //sanitize the profileId before searching
+      try {
+         $profileId = self::validateUuid($profileId);
+      } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+         throw (new \PDOException($exception->getMessage(), 0, $exception));
       }
+
+      //create a query template
+      $query = "SELECT profileId, accountProfileId FROM profile WHERE profileId = :profileId";
+      $statement = $pdo->prepare($query);
+
+      //bind the profile id to the place holder in the template
+      $parameters = ["profileId" => $profileId->getBytes()];
+      $statement->execute($parameters);
+
+      //grab the profile from mySQL
+      try {
+         $profile = null;
+         $statement->setFetchMode(\PDO::FETCH_ASSOC);
+         $row = $statement->fetch();
+         if($row !== false) {
+            $profile = new Profile($row["profileId"], $row["accountProfileId"]);
+         }
+      } catch(\Exception $exception) {
+         //if the row couldn't be converted, rethrow it
+         throw(new \PDOException($exception->getMessage(), 0, $exception));
+      }
+      return ($profile);
+   }
+
    /**
-    * Gets the profile by account
+    * Gets the profile by profile name
+    * This was not initially set up as a FK,
+    * but is being used as one now for the sake of this exercise
     *
     * @param \PDO $pdo PDO connection object
-    * @param \string $accountProfileId profile to search for
+    * @param \string $profileName profile to search for
     * @return \SplFixedArray SplFixedArray of profiles found
     * @throws \PDOException when mySQL errors occur
     * @throws \TypeError when variables are not the correct data type
     */
 
+   public static function getProfileByProfileName(\PDO $pdo, string $profileName): \SplFixedArray {
+      //sanitize the type before searching
+      $profileName = trim($profileName);
+      $profileName = filter_var($profileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      if(empty($profileName) === true) {
+         throw(new \PDOException("Profile name cannot be empty"));
+      }
+
+      //escape any mySQL wild cards
+      $profileName = str_replace("_", "\\_", str_replace("&", "\\&", $profileName));
+
+      //create query template
+      $query = "SELECT profileId, profileName FROM profile WHERE profileName LIKE :profileName";
+      $statement = $pdo->prepare($query);
 
 
-
-
-
+   }
 
 
 
